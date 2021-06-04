6bbcd007d63630db5fc9219de465afd40fdad233
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-04 09:12:37 +0530

    Refactored: to create board,added comments

--- TicTacToe_Game/src/game/TicTacToe.java
+++ TicTacToe_Game/src/game/TicTacToe.java
@@ -1,28 +1,18 @@
 package game;
 import java.util.Scanner;
 public class TicTacToe {
-	static char[] board= {' ',' ',' ',' ',' ',' ',' ',' ',' '};
-	public static void printBoard() {
-		        System.out.println("|---|---|---|");
-		        System.out.println("| " + board[0] + " | "
-		                           + board[1] + " | " + board[2]
-		                           + " |");
-		        System.out.println("|-----------|");
-		        System.out.println("| " + board[3] + " | "
-		                           + board[4] + " | " + board[5]
-		                           + " |");
-		        System.out.println("|-----------|");
-		        System.out.println("| " + board[6] + " | "
-		                           + board[7] + " | " + board[8]
-		                           + " |");
-		        System.out.println("|---|---|---|");
-		   
-		
+	static char[] board= new char[10];
+	public static void createBoard() {
+		        for (int i=1;i<board.length;i++)
+		        {
+		        	board[i]=' ';
+		        	System.out.println(i);
+		        }
 	}
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		printBoard();
+		createBoard();
 	    
 		
 
